6524da0aac4ad62b3dc09bd349e7ea82b6344438
NiklasJonsson6 <bknjniklas@gmail.com>
2015-11-19 09:46:00 +0100

    part 1 / 2

--- src/geometri/Circle.java
+++ src/geometri/Circle.java
@@ -5,9 +5,13 @@ import java.awt.*;
 public class Circle extends GeometricalObject {
     private int diameter;
 
-    public Circle(int x, int y, int diameter, Color color) {
+    public Circle(int x, int y, int diameter, Color color) throws IllegalPositionException {
         super(x, y, color, Math.pow(diameter / 2, 2) * Math.PI, diameter * Math.PI);
         this.diameter = diameter;
+    }
+
+    public Circle(GeometricalForm f, int diameter, Color color) throws IllegalPositionException {
+        this(f.getX(), f.getY(), diameter, color);
     }
 
     public int getWidth() {
